164aa816f4906cb13d6f95f4248a04ac9ea60de0
matteo-lang-detecon <matteoalessandro.lang@detecon.com>
2022-04-22 12:00:27 +0200

    adding db conncetion

--- docker/src/testapp.py
+++ docker/src/testapp.py
@@ -3,14 +3,64 @@
 # import necessary packages
 import os
 from flask import Flask
+import json
+import mysql.connector
 
 # create flask app
 flaskapp = Flask(__name__)
 
 # base route to check if service is running
-@flaskapp.route('/')
+@flaskapp.route('/alive')
 def alive():
-    return "200 OK"
+    return "I'm alive!"
+
+@app.route('/customers', methods=['GET'])
+def index():
+    conn = mysql.connector.connect(
+        host = os.environ.get("DB_HOST"), 
+        user = os.environ.get("DB_USER"),
+        passwd = os.environ.get("DB_PWD"),
+        database = "sample")  
+
+    #retrieving information 
+    cur = conn.cursor() 
+    cur.execute("SELECT * FROM customer")
+
+    # serialize results into JSON
+    row_headers=[x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+         json_data.append(dict(zip(row_headers,result)))
+    
+    conn.close()
+    
+    # return the results
+    return json.dumps(json_data)
+
+@app.route('/agents', methods=['GET'])
+def index():
+    conn = mysql.connector.connect(
+        host = os.environ.get("DB_HOST"), 
+        user = os.environ.get("DB_USER"),
+        passwd = os.environ.get("DB_PWD"),
+        database = "sample")  
+
+    #retrieving information 
+    cur = conn.cursor() 
+    cur.execute("SELECT * FROM agents")
+
+    # serialize results into JSON
+    row_headers=[x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+         json_data.append(dict(zip(row_headers,result)))
+    
+    conn.close()
+    
+    # return the results
+    return json.dumps(json_data)
    
 # run the app
 if __name__ == '__main__':
